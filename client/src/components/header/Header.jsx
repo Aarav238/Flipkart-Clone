f7a3535cbbf40a51e814ea3e5711b833b99f2e6b
Aarav238 <aarav8090shukla@gmail.com>
2023-04-27 15:52:06 +0530

    logo added

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {AppBar , Toolbar, styled , Box, Typography} from "@mui/material" 
+import Search from './Search';
 
 
 const StyledHeader = styled(AppBar)`
@@ -10,23 +11,36 @@ const Component = styled(Box)`
 margin-left: 12%;
 line-height: 0;
 `
+const SubHeading = styled(Typography)`
+font-size: 10px;
+font-style: italic;
+`
+//handle css of html tags with styled components
+const PlusImage = styled("img")({
+    width: 10,
+    height: 10,
+    marginLeft: 4
+}
+
+)
 
 const Header = () => {
-    const logo = "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png"
+    const logo = "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
   return (
     
         <StyledHeader>
         <Toolbar>
           <Component>
-           <img src={logo} alt="logo" style={{
-            width: 75
-           }} /> 
-           <Box>
-            <Typography>
-                Explore Plus
-            </Typography>
+           <img src={logo} alt="logo" style={{ width: 75 }} /> 
+           <Box style={{ display : 'flex'}}>
+            <SubHeading>Explore&nbsp;
+                <Box component="span" style={{ color: "#FFE500" }}>Plus</Box>
+                </SubHeading>
+                <PlusImage src={subURL} alt="sublogo" />
            </Box>
           </Component>
+          <Search/>
         </Toolbar>
     </StyledHeader>
     
