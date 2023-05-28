93602b5cfafeb917d98aed66350407f5906879c9
Aarav238 <aarav8090shukla@gmail.com>
2023-05-29 02:49:39 +0530

    product page

--- client/src/components/details/DetailView.jsx
+++ client/src/components/details/DetailView.jsx
@@ -1,21 +1,40 @@
 import { useEffect } from "react";
 
-import { useDispatch } from "react-redux";
+import { useDispatch , useSelector  } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProductDetails } from "../../redux/actions/productAction";
+import ActionItem from "./ActionItem";
+import { Box, Typography ,styled , Grid } from "@mui/material";
 
+const  Component = styled(Box) `
+background: #F2F2F2;
 
+`
 const DetailView = () => {
     const dispatch = useDispatch();
     const {id} = useParams();
 
+    const {loading , product } = useSelector(state => state.getProductDetails);
     useEffect(() =>{
-        dispatch(getProductDetails(id))
+        if( product && id !== product.id)
+         dispatch(getProductDetails(id))
 
-    },[dispatch, id])
-
+    },[dispatch, id, product , loading])
+    console.log(product)
     return (
-        <div>hello</div>
+        <Component>
+            { product && Object.keys(product).length &&
+            <Grid>
+            <Grid>
+                <ActionItem product = {product}/> 
+            </Grid>
+            <Grid>
+                <Typography>{product.title.longTitle}</Typography>
+            </Grid>
+
+            </Grid>
+            }
+        </Component>
     )
 }
 
