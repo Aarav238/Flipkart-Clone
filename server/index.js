748652edf7f7bfcfd8ea7f70abc73671fe1fa85f
Aarav238 <aarav8090shukla@gmail.com>
2023-07-03 21:32:16 +0530

    unneccessary files removed

--- server/index.js
+++ server/index.js
@@ -27,15 +27,3 @@ app.listen(PORT, () =>
 console.log(`server is running on port ${PORT}`)
 )
 //defaultData();
-export let paytmParams = {
-    MID: process.env.PAYTM_MID,
-    WEBSITE: process.env.PAYTM_WEBSITE,
-    CHANNEL_ID: process.env.PAYTM_CHANNEL_ID,
-    INDUSTRY_TYPE_ID: process.env.PAYTM_INDUSTRY_TYPE_ID,
-    ORDER_ID: uuid(),
-    CUST_ID: process.env.PAYTM_CUST_ID,
-    TXN_AMOUNT: '100',
-    CALLBACK_URL: 'http://localhost:3000/callback',
-    EMAIL: 'aarav8090shukla@gmail.com',
-    MOBILE_NO: '1234567852'
-  };
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { userSignUp ,userLogIn } from "../controllers/userController.js";
 import { getPrducts , getProductById} from "../controllers/productsController.js";
-import { addPaymentGateway, paymentResponse } from "../controllers/payment-controller.js";
 const router = express.Router();
 
 router.post('/signup',userSignUp);
@@ -11,7 +10,4 @@ router.post('/login',userLogIn)
 
 router.get('/products', getPrducts);
 router.get('/product/:id', getProductById)
-
-router.post('/payment',addPaymentGateway)
-router.post('/callback',paymentResponse)
 export default router;
