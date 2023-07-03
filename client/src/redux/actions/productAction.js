4bc132fe6f283af7ecdf1e13560c748805908438
Aarav238 <aarav8090shukla@gmail.com>
2023-07-03 23:12:56 +0530

    url updated

--- client/src/redux/actions/productAction.js
+++ client/src/redux/actions/productAction.js
@@ -22,7 +22,7 @@ export const getProduct = () => async(dispatch) => {
 export const getProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: actionType.GET_PRODUCTS_DETAILS_REQUEST });
-        const { data } = await axios.get(`http://localhost:5000/product/${id}`);
+        const { data } = await axios.get(`https://clone-backend-jslt.onrender.com/product/${id}`);
         
         dispatch({ type: actionType.GET_PRODUCTS_DETAILS_SUCCESS, payload: data });
 
--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:5000"
+const URL = "https://clone-backend-jslt.onrender.com"
 
 export const authenticateSignup = async (data) => {
     try {
